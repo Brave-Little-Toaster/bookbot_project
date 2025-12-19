e1917e756a859327bf5521a0ab9ffd44d0681bfe
Brave-Little-Toaster <loganpull@gmail.com>
2025-12-18 22:58:14 -0600

    Added functionality to view all char in book as list

--- stats.py
+++ stats.py
@@ -12,3 +12,16 @@ def number_of_characters(printed_book):
             char_counts[letter] = 0
         char_counts[letter] += 1
     return char_counts
+
+
+def chars_dict_to_sorted_list(char_counts):
+    sorted_chars = []
+    for char, count in char_counts.items():
+        combo = {"char": char, "num": count}
+        sorted_chars.append(combo)
+    sorted_chars.sort(reverse=True, key=sort_on)
+    return sorted_chars
+
+
+def sort_on(item):
+    return item["num"]
